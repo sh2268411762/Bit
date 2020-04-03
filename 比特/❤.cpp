227dcb3d69134b97fcf31043af8ed84d4e62178e
sh2268411762 <2268411762@qq.com>
2020-04-04 02:08:24 +0800

    比特

---
+++ 比特/❤.cpp
@@ -0,0 +1,10 @@
+/*
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	printf("   ***    ***\n**     **     **\n  **       **\n     **  **\n       **\n\nhello!\ndo you want to be my girlfriend?¡®_¡¯\n");
+	system("pause");
+	return 0;
+}
+*/
--- 比特/调试.c
+++ 比特/调试.c
@@ -3,11 +3,31 @@
 int main()
 {
 	int i = 0;
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
 	for (i = 0; i < 10; i++)
 	{
-		printf("%d ", i);
+		printf("%d ", arr[i]);
+		printf("hehe\n");
 	}
+	/*for (i = 0; i < 10; i++)
+	{
+		arr[i] = 0;
+		printf("hehe\n");
+	}*/
+	/*printf("%p\n", &i);
+	printf("%p\n", arr);*/
 	return 0;
 }
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", i);
+//	}
+//	return 0;
+//}
 //Debug   调试版本，包含调试信息，并且不做任何优化，便于程序员调试程序
 //Release 发布版本，进行了各种优化，使得程序在代码大小和运行速度上都是最优的，以便用户很好地使用
+//条件断点  创建断点，右击，选择条件
